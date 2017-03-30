82ca5818709fce2ab21f6bfe45dc54feb71f6623
geoffreyhale <geoffreyhale@gmail.com>
2017-03-29 23:55:29 -0700

    add Post form

--- src/OneThirtyWordsBundle/Controller/DefaultController.php
+++ src/OneThirtyWordsBundle/Controller/DefaultController.php
@@ -2,9 +2,13 @@
 
 namespace OneThirtyWordsBundle\Controller;
 
+use OneThirtyWordsBundle\Entity\Post;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -12,9 +16,39 @@ class DefaultController extends Controller
      * @Route("/", name="homepage")
      * @Template("OneThirtyWordsBundle:Default:index.html.twig")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $user = $this->getUser();
-        return ['user' => $user];
+
+        if ($user) {
+            $em = $this->getDoctrine()->getManager();
+
+            $post = $em->getRepository(Post::class)->findOneBy(['date' => new \DateTime('today')]);
+
+            if (!$post) {
+                $post = new Post();
+                $post->setDate(new \DateTime);
+            }
+
+            $form = $this->createFormBuilder($post)
+                ->add('body', TextareaType::class)
+                ->add('submit', SubmitType::class, array('label' => 'Submit Post'))
+                ->getForm();
+
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $data = $form->getData();
+
+                $em->persist($data);
+                $em->flush();
+            }
+        }
+
+        return [
+            'form' => $form->createView(),
+            'date' => $post->getDate()->format('l, F jS'),
+            'user' => $user,
+        ];
     }
 }
