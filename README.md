da34fa5fb1b3bb31478107f4842d7f37e6b00118
geoffreyhale <geoffreyhale@gmail.com>
2017-03-29 22:42:49 -0700

    User login

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 ======
 
 A secure journaling website.
+
+Deploy
+------
+
+`composer install;`
+`bin/console doctrine:schema:update --force;`
+`bin/console cache:clear`
+`bin/console server:start`
--- src/CronkdBundle/Controller/DefaultController.php
+++ src/CronkdBundle/Controller/DefaultController.php
@@ -14,6 +14,7 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return ['something' => 'value'];
+        $user = $this->getUser();
+        return ['user' => $user];
     }
 }
