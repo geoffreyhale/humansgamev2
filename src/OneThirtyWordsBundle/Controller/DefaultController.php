25b9e8aa82229d2006c733fe6a8d50ca777462b0
geoffreyhale <geoffreyhale@gmail.com>
2017-06-10 21:35:36 -0700

    add post edit and change home page

--- src/OneThirtyWordsBundle/Controller/DefaultController.php
+++ src/OneThirtyWordsBundle/Controller/DefaultController.php
@@ -2,9 +2,7 @@
 
 namespace OneThirtyWordsBundle\Controller;
 
-use OneThirtyWordsBundle\Entity\Category;
 use OneThirtyWordsBundle\Entity\Post;
-use OneThirtyWordsBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -18,58 +16,32 @@ class DefaultController extends Controller
      * @Route("/", name="homepage")
      * @Template("OneThirtyWordsBundle:Default:index.html.twig")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
 
-        /** @var User $user */
-        $user = $this->getUser();
+        $posts = $em->getRepository(Post::class)->findBy([
+            'date' => new \DateTime('today'),
+            'user' => $this->getUser(),
+        ]);
 
-        if ($user) {
-            if ($user->getCategories()->isEmpty()) {
-                $category = (new Category())
-                    ->setName('Default')
-                    ->setUser($user);
-                $em->persist($category);
-                $em->flush();
-            }
-
-            /** @var Post $post */
-            $post = $em->getRepository(Post::class)->findOneBy([
-                'date' => new \DateTime('today'),
-                'user' => $user,
-            ]);
-
-            if (!$post) {
-                $post = new Post();
-                $post->setDate(new \DateTime);
-            }
-
-            if (!$post->getCategory()) {
-                $post->setCategory($user->getCategories()->first());
-            }
-
-            $form = $this->createFormBuilder($post)
-                ->add('body', TextareaType::class)
-                ->add('submit', SubmitType::class, array('label' => 'Submit Post'))
-                ->getForm();
-
-            $form->handleRequest($request);
-
-            if ($form->isSubmitted() && $form->isValid()) {
-                /** @var Post $data */
-                $data = $form->getData()
-                    ->setCategory($user->getCategories()->first())
-                    ->setUser($user);
-
-                $em->persist($data);
-                $em->flush();
-            }
+        $wordcount = 0;
+        foreach ($posts as $post) {
+            $wordcount += str_word_count($post->getBody());
         }
 
+        $categories = $this->getUser()->getCategories()->getValues();
+
+        uasort($categories, function ($c1, $c2) {
+            if ($c2->getName() == $c1->getName()) return 0;
+            return $c2->getName() > $c1->getName() ? -1 : 1;
+        });
+
         return [
-            'form' => $form->createView(),
-            'post' => $post,
+            'categories' => $categories,
+            'posts' => $posts,
+            'user' => $this->getUser(),
+            'wordcount' => $wordcount,
         ];
     }
 }
--- src/OneThirtyWordsBundle/Controller/PostController.php
+++ src/OneThirtyWordsBundle/Controller/PostController.php
@@ -2,10 +2,13 @@
 
 namespace OneThirtyWordsBundle\Controller;
 
+use OneThirtyWordsBundle\Entity\Category;
 use OneThirtyWordsBundle\Entity\Post;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Request;
 
 class PostController extends Controller
@@ -44,5 +47,77 @@ class PostController extends Controller
         return [
             'posts' => array_reverse($posts),
         ];
+    }
+
+    /**
+     * @Route("/post/{id}/edit", name="postEdit")
+     * @Template("OneThirtyWordsBundle:Post:edit.html.twig")
+     */
+    public function postEditAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $post = $em->getRepository(Post::class)->findOneBy([
+            'id' => $id
+        ]);
+
+        if ($post->getUser() !== $this->getUser()) {
+            throw new \Exception("ACCESS DENIED: This is not your post.");
+        }
+
+        if ($post->getDate()->format('Ymd') != (new \DateTime)->format('Ymd')) {
+            throw new \Exception("CANNOT EDIT: You can only edit posts written today.");
+        }
+
+        $form = $this->createFormBuilder($post)
+            ->add('body', TextareaType::class)
+            ->add('submit', SubmitType::class, array('label' => 'Submit Post'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $em->persist($post);
+            $em->flush();
+        }
+
+        return [
+            'form' => $form->createView(),
+            'post' => $post,
+        ];
+    }
+
+    /**
+     * @Route("/new-post-by-category/{category_id}", name="newPostByCategory")
+     * @Template("OneThirtyWordsBundle:Post:edit.html.twig")
+     */
+    public function newPostByCategoryAction($category_id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $category = $em->getRepository(Category::class)->find($category_id);
+
+        if ($category->getUser() !== $this->getUser()) {
+            throw new \Exception("ACCESS DENIED: This is not your category.");
+        }
+
+        $post = $em->getRepository(Post::class)->findOneBy([
+            'category' => $category,
+            'date' => new \DateTime('today'),
+        ]);
+
+        if (!$post) {
+            $post = (new Post())
+                ->setCategory($category)
+                ->setDate(new \DateTime)
+                ->setUser($this->getUser());
+            $em->persist($post);
+            $em->flush();
+        }
+
+        return $this->forward('OneThirtyWordsBundle:Post:postEdit', array(
+            'id'  => $post->getId(),
+        ));
     }
 }
